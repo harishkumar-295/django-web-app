d063d9659e5963a83c272477226c811aebdaffa9
harishkumar-295 <coolboyghkumar295@gmail.com>
2022-10-31 23:27:43 +0530

    completed upto model creation

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,27 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-    "author":"Harish Kumar",
-    "title":"Blog Post 1",
-    "content":"first post content",
-    "created_at":"Oct 30, 2022"
-    },
-    {
-    "author":"Harish Kumar sa",
-    "title":"Blog Post 2",
-    "content":"second post content",
-    "created_at":"Oct 30, 2022"
-    }
-    ]
+# posts = [
+#     {
+#     "author":"Harish Kumar",
+#     "title":"Blog Post 1",
+#     "content":"first post content",
+#     "created_at":"Oct 30, 2022"
+#     },
+#     {
+#     "author":"Harish Kumar sa",
+#     "title":"Blog Post 2",
+#     "content":"second post content",
+#     "created_at":"Oct 30, 2022"
+#     }
+#     ]
 
 # Create your views here.
 def home(request):
     context = {
-        "posts": posts
+        "posts": Post.objects.all()
     }
     return render(request,'blog/home.html',context)
 
 def about(request):
-    return render(request,'blog/about.html')
+    return render(request,'blog/about.html',{'title':'About'})
