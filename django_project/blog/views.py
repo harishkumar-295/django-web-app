07a99b67f3ffdefed442f1a2bb4107bb61111c56
harishkumar-295 <coolboyghkumar295@gmail.com>
2022-11-12 15:20:50 +0530

    completed all features

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from .models import Post
+from django.contrib.auth.models import User
 from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 
@@ -32,6 +33,18 @@ class PostListView(ListView):
     # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-created_at']
+    paginate_by: int = 2
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'
+    # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by: int = 2
+    
+    def get_queryset(self):
+        user = get_object_or_404(User,username = self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-created_at')
     
 class PostDetailView(DetailView):
     model = Post
--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -128,6 +128,12 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL')
+EMAIL_HOST_PASSWORD = os.getenv('PASSWORD')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
